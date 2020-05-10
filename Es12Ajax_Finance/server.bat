3d2a2d49404ba8eb0d9ca4da0fee9ab4a81d7096
GitDeepak-C <61829381+GitDeepak-C@users.noreply.github.com>
2020-05-10 19:29:42 +0200

    Caricamento con correzzioni dell'esercizio n°12-Finance

---
+++ Es12Ajax_Finance/server.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+/*npm install http-server
+/*http-server C:\Users\Deepak\Desktop\Es12Ajax_Finance -p 8686
+/*start http://localhost:8686/
+json-server --watch db.json
+ECHO SERVER INSTALLATO
+PAUSE
